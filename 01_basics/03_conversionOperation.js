a6c6058d1a46720483d0c7c67edc615500da78b1
Aftabkhan17 <aftabkhan.m.m.k@gmail.com>
2024-11-27 19:22:07 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof (score))
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc"= NaN
+// True => 1; false => 0
+
+let isLoggedIn = "Aftab"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false 
+// "Aftab"=> true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
