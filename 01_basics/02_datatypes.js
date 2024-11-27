047d3307624a60e38d9e2f2552c0b72fbe7dc558
Aftabkhan17 <aftabkhan.m.m.k@gmail.com>
2024-11-27 18:46:58 +0000

    We learned about datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS as newer version
+
+// alert("3 + 3") we are using nodejs, not browser
+
+console.log(3
+    +
+     3) // code rediability should be high
+console.log("Aftab")
+
+
+let name = "Aftab"
+let age = 21 
+let isloggedIn = false 
+// number => 2 to the power 53
+//  bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+
+// object
+
+console.log(typeof undefind); // undefined
+console.log(typeof null); // object
