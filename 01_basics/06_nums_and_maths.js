a5b21fa81bcff23811203a0c6074343cb41bf260
Aftabkhan17 <aftabkhan.m.m.k@gmail.com>
2024-12-01 18:30:36 +0000

    mathandnum

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,42 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.3595
+
+// console.log(otherNumber.toPrecision(7));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ****************  Maths  *********************
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.sqrt(25));
+// console.log(Math.max(25, 30, 45, 55 ));
+// console.log(Math.min(25, 30, 45, 55 ));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 10
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
