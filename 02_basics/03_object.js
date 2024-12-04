83fc8066f868b27dfb7c6ce934e73ab7f9350162
Aftabkhan17 <aftabkhan.m.m.k@gmail.com>
2024-12-04 17:52:10 +0000

    objects in js

---
+++ 02_basics/03_object.js
@@ -0,0 +1,12 @@
+// singleton
+//objecct literals
+
+const JsUser = {
+    name : "Aftab",
+    age: 22,
+    locatiion: Boisar
+    emial: "aftab@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+console.log(JsUser.email)
