25e8e7e37263a1bfe9506defc123162fccdd8b94
Aftabkhan17 <aftabkhan.m.m.k@gmail.com>
2024-12-04 18:34:08 +0000

    objects in js

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,12 +1,35 @@
 // singleton
 //objecct literals
 
+const mySym = Symbol("key1")
+
+
 const JsUser = {
-    name : "Aftab",
+    name: "Aftab",
+    "full name": "Aftab Khan",
+    [mySym]: "myKey1",
     age: 22,
-    locatiion: Boisar
-    emial: "aftab@google.com",
+    locatiion: "Boisar",
+    email: "aftab@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
-console.log(JsUser.email)
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+
+JsUser.email = "aftab@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "aftab@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log('Hello JS user", ${this.name}');
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
