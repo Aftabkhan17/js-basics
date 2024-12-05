4dbb46856cdfdb3012a3d29e8010c3f7089c2c08
Aftabkhan17 <aftabkhan.m.m.k@gmail.com>
2024-12-05 09:44:07 +0000

    objects in js

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,25 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.lastname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj1 = {3: "a", 4: "b"}
+
+
+
