98cf0d315adbdbd77d5c87144efaa54193e88ff2
Aftabkhan17 <aftabkhan.m.m.k@gmail.com>
2024-12-08 05:25:48 +0000

    functions with object

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -35,7 +35,37 @@ function loginUserMessage(username = "sam") {
     return '${username} just logged in'
 }
 // console.log(loginUserMessage("Aftab"))
-console.log(loginUserMessage("Aftab"))
+// console.log(loginUserMessage("Aftab"))
 
+
+function calculateCartPrice(val1, val2, ...num1){  //Rest Operator ...num1
+    return num1
+
+}
+//console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user ={
+    username: "aftab",
+    prices: 199
+}
+
+function hadleObject(anyobject){
+    // console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+//hadleObject(user)
+hadleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnFirstValue(getArray){
+    return getArray[0]
+
+}
+// console.log(returnFirstValue(myNewArray));
+console.log(returnFirstValue([200, 400, 100, 600]));
 
 
